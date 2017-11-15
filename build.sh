a1df6d599127efa6aed58b4a5b2491e669a3960f
TTS기술담당 John berrybryan <skt.1902654@sk.com>
2017-11-15 18:18:30 +0900

    Forgot to update the parent build script

--- build.sh
+++ build.sh
@@ -6,9 +6,20 @@ cd base
 cd ../name_node
 ./build.sh
 
+cd ../secondary_name_node/
+./build.sh
+
 cd ../resource_manager
+./build.sh
+
+cd ../node_manager
 ./build.sh
 
 cd ../data_node
 ./build.sh
 
+cd ../job_history
+./build.sh
+
+cd ../web_app_proxy
+./build.sh
